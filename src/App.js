6f184748bfc512cb59a183b9d4ad7f836fbc3fb6
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-09-03 11:57:37 -0700

    useInput

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Count from "./hooks/useState";
+import UseInputApp from "./customHooks/useInput";
+
+
+
+
 
 const App = () => {
   return (
-    <>
-      <h2>Custom Hooks</h2>
-      <Count />
-    </>
+    <div>
+      <h1>Custom Hooks</h1>
+      <UseInputApp />
+    </div>
   )
 }
 
---
+++ src/customHooks/useInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = event => {
+        const {
+            target: { value }
+        } = event;
+        let willUpdate = true;
+        if (typeof validator === "function") {
+            willUpdate = validator(value)
+        }
+        if (willUpdate) {
+            setValue(value);
+        }
+    }
+
+    return { value, onChange };
+}
+
+const UseInputApp = () => {
+    const maxLen = (value) => value.length <= 10;
+    const name = useInput("Mr.", maxLen)
+
+    return (
+        <div>
+            <h2>useInput</h2>
+            <h4>Hello, {name.value}</h4>
+            <input placeholder="Name" {...name} />
+        </div>
+    )
+}
+
+export default UseInputApp;
